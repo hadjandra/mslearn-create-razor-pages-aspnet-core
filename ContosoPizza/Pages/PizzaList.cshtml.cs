4eb80868bba127cd425cd2cc93479bfefc12a0f9
HADJANDRA SOBRINHO RIBEIRO DA SILVA <hadjandra.srsilva@senacsp.edu.br>
2023-07-27 17:18:51 -0300

    autualização 27/07/23

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoPizza.Models;
 using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ContosoPizza.Pages
 {
     public class PizzaListModel: PageModel
     {
+        [BindProperty]
+        public Pizza Newpizza { get; set; } = default!;
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
@@ -18,6 +21,23 @@ namespace ContosoPizza.Pages
         public void OnGet()
         {
             PizzaList= _service.GetPizzas();
+        }
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid || Newpizza==null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(Newpizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id) 
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
         }
     }
 }
